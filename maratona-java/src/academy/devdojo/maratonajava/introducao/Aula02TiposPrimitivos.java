60de19b32ef5b0510b74335aec1f26d03dce3f07
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2026-01-26 16:35:18 -0300

    Add example code for primitive types and variables

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+
+        System.out.println("A idade é " + idade + " anos");
+        System.out.println(verdadeiro);
+        System.out.println("char " + caractere);
+    }
+}
