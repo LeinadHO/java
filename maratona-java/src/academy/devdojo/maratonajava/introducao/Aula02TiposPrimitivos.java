dc0d5656716722edcbd0e66245048a794e151d6b
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2026-02-01 16:27:31 -0300

    Add examples for String, casting and type specification

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,20 +1,23 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int idade = (int) 10000000000L;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
-        char caractere = 65;
+        char caractere = '\u0041';
+        String nome = "Carimbo Miguel";
 
         System.out.println("A idade é " + idade + " anos");
         System.out.println(verdadeiro);
         System.out.println("char " + caractere);
+        System.out.println(idade);
+        System.out.println("Oi, meu nome é " + nome);
     }
 }
