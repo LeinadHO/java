8f416a74dee0bb974c5d57744e6abede3f9257df
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2026-02-02 15:55:26 -0300

    Add example code for arithmetic and relational operators

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.sql.SQLOutput;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero1 = 10;
+        int numero2 = 20;
+        double resultado = numero1 / (double) numero2;
+        System.out.println(resultado);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte: " + isDezIgualVinte);
+        System.out.println("isDezIgualDez: " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez: " + isDezDiferenteDez);
+    }
+}
