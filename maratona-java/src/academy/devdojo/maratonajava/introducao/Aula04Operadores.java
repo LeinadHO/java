e1061bf16901c3763b5af993b2b103912dba8b0d
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2026-02-05 15:36:43 -0300

    Add notes for 6.3 and 6.4

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -25,5 +25,39 @@ public class Aula04Operadores {
         System.out.println("isDezIgualVinte: " + isDezIgualVinte);
         System.out.println("isDezIgualDez: " + isDezIgualDez);
         System.out.println("isDezDiferenteDez: " + isDezDiferenteDez);
+
+        // && (AND), || (OR), ! (NOT)
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta: " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta: " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel: " + isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2; // 3600
+        bonus /= 2; // 1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(contador2++);
+        System.out.println(++contador2);
     }
 }
